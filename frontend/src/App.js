2ca2f53951a7e22dc437d5e2030427b1160c4fd7
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-12-26 23:53:15 +0100

    mobile ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,12 @@
 import "./App.scss";
 import Nav from "./components/navBar";
+import Home from "./components/home";
 
 function App() {
   return (
     <body>
       <Nav />
-
-      <main>
-        <h1 className={"main-title"}>Welcome to Make-a-Mess</h1>
-        <p>Tired of not knowing what you can or cannot eat ?</p>
-        <h2>Let us help you!</h2>
-        <a href="#">Start Building</a>
-        <div className={"bg-burger"}></div>
-      </main>
+      <Home />
     </body>
   );
 }
--- frontend/src/components/navBar.js
+++ frontend/src/components/navBar.js
@@ -1,5 +1,5 @@
 import logo from "../assets/images/logo-burger.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function BurgerMenu() {
   const [burgerClass, setBurgerClass] = useState("burger-bar unclicked");
@@ -7,14 +7,26 @@ export default function BurgerMenu() {
   const [menuClass, setMenuClass] = useState("menu hidden");
   const [menuIndex, setMenuIndex] = useState("nav-menu hidden");
   const [toggleBurger, setToggleBurger] = useState(false);
+  const [stickyCLass, setStickyClass] = useState(false);
+  const [moveLogo, setMoveLogo] = useState("");
+
+  useEffect(() => {
+    const stickNavbar = () => {
+      setStickyClass(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", stickNavbar);
+    return () => window.removeEventListener("scroll", stickNavbar);
+  }, []);
 
   const onClickToggle = function () {
     if (!toggleBurger) {
+      setMoveLogo("move-in");
       setBurgerClass("burger-bar clicked");
       setMenuClass("menu visible");
       setNavClass("nav gray");
       setMenuIndex("nav-menu visible");
     } else {
+      setMoveLogo("move-out");
       setBurgerClass("burger-bar unclicked");
       setMenuClass("menu hidden");
       setNavClass("nav red");
@@ -23,9 +35,9 @@ export default function BurgerMenu() {
     setToggleBurger(!toggleBurger);
   };
   return (
-    <div>
+    <div className={`${stickyCLass ? "sticky" : ""}`}>
       <nav className={navClass}>
-        <img src={logo} alt="logo du site Make-a-Mess" />
+        <img src={logo} alt="logo du site Make-a-Mess" className={moveLogo} />
         <div className={"burger-menu"} onClick={onClickToggle}>
           <div className={burgerClass}></div>
           <div className={burgerClass}></div>
@@ -33,6 +45,17 @@ export default function BurgerMenu() {
         </div>
         <div className={menuClass}>
           <ul className={menuIndex}>
+            <a className={"nav-option"}>Home</a>
+            <a className={"nav-option"}>Need Ideas?</a>
+            <a className={"nav-option"}>About Us</a>
+            <a className={"nav-option"}>Contact Us</a>
+          </ul>
+          <a className={"button"} href="#">
+            Start Building
+          </a>
+        </div>
+        <div className={`${stickyCLass ? "sticky" : ""}`}>
+          <ul className={"nav-tablet-desktop"}>
             <a className={"nav-option"}>Home</a>
             <a className={"nav-option"}>Need Ideas?</a>
             <a className={"nav-option"}>About Us</a>
