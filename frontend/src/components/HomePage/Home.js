b48bc455f11bf91c35eef695ba3deece502e556b
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2023-12-29 11:48:48 +0100

    opacity animation on HomePage

--- frontend/src/components/HomePage/Home.js
+++ frontend/src/components/HomePage/Home.js
@@ -5,9 +5,9 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <motion.div
-      initial={{ width: 0 }}
-      animate={{ width: "100%" }}
-      exit={{ x: window.innerWidth, transition: "0.2s" }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
     >
       <NavBarProvider>
         <OnClickToggle />
