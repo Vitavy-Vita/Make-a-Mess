f6b5582286448aff72cd97706e8e905deb37c9df
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2024-01-07 21:15:43 +0100

    vercel test

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRouter.js";
 import cors from "cors";
 const app = express();
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -22,3 +23,4 @@ app.use(userRouter);
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on port: ${process.env.BASE_URL}`);
 });
+
