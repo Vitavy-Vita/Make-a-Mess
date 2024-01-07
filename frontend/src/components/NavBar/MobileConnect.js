8194252a25165f92c9e38dfbc4bebd61adb0c6a1
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2024-01-07 11:51:11 +0100

    fixed design

--- frontend/src/components/NavBar/MobileConnect.js
+++ frontend/src/components/NavBar/MobileConnect.js
@@ -3,13 +3,13 @@ import { useNavBarProvider } from "../../context/NavBarContext";
 export default function MobileConnect() {
   const value = useNavBarProvider()
   return (
-    <div className={"mobile-connect"}>
+    <aside className={"mobile-connect"}>
       <section className={value.connectSlide}>
         <div className="buttons">
           <a href="/CreateAccount">New Account</a>
           <a href="/Login">Login</a>
           </div>
       </section>
-    </div>
+    </aside>
   );
 }
