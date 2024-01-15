e80f572ca6720d780375983d5624bebe590c6d49
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2024-01-15 20:39:12 +0100

    responsive stuff

--- frontend/src/components/Dashboard/DashboardAdmin.js
+++ frontend/src/components/Dashboard/DashboardAdmin.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const DashboardAdmin = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <h2>Databases:</h2>
-           <NavLink to={"/update/bread"}>Bread</NavLink>
-           <NavLink to={"/update/cheese"}>Cheese</NavLink>
-           <NavLink to={"/update/meat"}>Meat</NavLink>
-           <NavLink to={"/update/sauce"}>Sauce</NavLink>
-           <NavLink to={"/update/topping"}>Topping</NavLink>
-           <NavLink to={"/burgers/new"}>Pre-Made</NavLink>
-           <NavLink to={"/users"}>Users</NavLink>
-        </div>
-    );
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      <h2>Databases:</h2>
+      <section className="database-container">
+        <NavLink to={"/update/bread"}>Bread</NavLink>
+        <NavLink to={"/update/cheese"}>Cheese</NavLink>
+        <NavLink to={"/update/meat"}>Meat</NavLink>
+        <NavLink to={"/update/sauce"}>Sauce</NavLink>
+        <NavLink to={"/update/topping"}>Topping</NavLink>
+        <NavLink to={"/burgers/new"}>Pre-Made</NavLink>
+        <NavLink to={"/users"}>Users</NavLink>
+      </section>
+    </main>
+  );
 };
 
 export default DashboardAdmin;
