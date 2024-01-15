fe4edd2f6efa9d4a5587785de94d06785ad9db4f
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2024-01-15 20:57:43 +0100

    stuff

--- frontend/src/components/NavBar/Login.js
+++ frontend/src/components/NavBar/Login.js
@@ -1,5 +1,6 @@
 import { FaRegUserCircle } from "react-icons/fa";
 import { IoIosLogIn } from "react-icons/io";
+import { IoIosSettings } from "react-icons/io";
 import { MdOutlineManageAccounts } from "react-icons/md";
 import { useNavBarProvider } from "../../context/NavBarContext";
 import { NavLink } from "react-router-dom";
@@ -14,10 +15,13 @@ export default function Login() {
           <NavLink to={"/CreateAccount"}>
             <MdOutlineManageAccounts className="form-icon" /> New Account
           </NavLink>
-          <NavLink to={"/Login"} >
+          <NavLink to={"/Login"}>
             <IoIosLogIn className="form-icon" /> Login
           </NavLink>
-          <NavLink to={"/Settings/Admin"} >Settings</NavLink>
+          <NavLink to={"/Settings/Admin"}>
+            <IoIosSettings className="form-icon" />
+            Settings
+          </NavLink>
         </article>
       </section>
     </aside>
--- frontend/src/components/NavBar/MobileConnect.js
+++ frontend/src/components/NavBar/MobileConnect.js
@@ -1,16 +1,30 @@
 import { NavLink } from "react-router-dom";
 import { useNavBarProvider } from "../../context/NavBarContext";
+import { IoIosLogIn } from "react-icons/io";
+import { IoIosSettings } from "react-icons/io";
+import { MdOutlineManageAccounts } from "react-icons/md";
 
 export default function MobileConnect() {
-  const value = useNavBarProvider()
+  const value = useNavBarProvider();
   return (
     <aside className={"mobile-connect"}>
       <section className={value.connectSlide}>
         <article className="buttons">
-          <NavLink to={"/CreateAccount"} onClick={value.onClickSlide}>New Account</NavLink>
-          <NavLink to={"/Login"} onClick={value.onClickSlide}>Login</NavLink>
-          <NavLink to={"/Settings/Admin"} onClick={value.onClickSlide}>Settings</NavLink>
-          </article>
+          <NavLink to={"/CreateAccount"} onClick={value.onClickSlide}>
+            {" "}
+            <MdOutlineManageAccounts className="form-icon" />
+            New Account
+          </NavLink>
+          <NavLink to={"/Login"} onClick={value.onClickSlide}>
+            {" "}
+            <IoIosLogIn className="form-icon" />
+            Login
+          </NavLink>
+          <NavLink to={"/Settings/Admin"} onClick={value.onClickSlide}>
+            <IoIosSettings className="form-icon" />
+            Settings
+          </NavLink>
+        </article>
       </section>
     </aside>
   );
